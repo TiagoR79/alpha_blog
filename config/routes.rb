65eadc5e8b29a5af32fb2d0a2b4b0b493baec172
TiagoR79 <Tiagor79@protonmail.com>
2022-11-09 11:29:10 +0000

    finished user signup - working

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 	resources :articles
   get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 	#resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
