ab69e69eb58cbcab73c0f33c0a169848992fa8ac
TiagoR79 <Tiagor79@protonmail.com>
2022-11-14 11:54:03 +0000

    used helper_methods on application_controller.rb to create user association across pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        #To not query the DB ever time -> use current_user or query 
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
+    def logged_in?
+        #transform current_user in bool using '!!'
+        !!current_user
+    end
+
 end
