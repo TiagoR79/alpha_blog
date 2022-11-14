41dbc9f732f40ae00e7de439e800c2f7278d598c
TiagoR79 <Tiagor79@protonmail.com>
2022-11-14 11:06:53 +0000

    finished basic user login and authentication - working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+
+    before_action :set_user
     
     def show
-        @user = User.find(params[:id])
         @articles = User.find(params[:id]).articles
     end
 
@@ -13,12 +14,10 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
-    def edit
-       @user = User.find(params[:id]) 
+    def edit 
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Account updated successfully"
             redirect_to @user
@@ -30,6 +29,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to Alpha Blog #{ @user.username }"
             redirect_to articles_path
         else
@@ -40,5 +40,9 @@ class UsersController < ApplicationController
     private 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
