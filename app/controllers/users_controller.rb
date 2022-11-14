aa94136b9840336358b44bdec7acea7e6c4898c1
TiagoR79 <Tiagor79@protonmail.com>
2022-11-14 14:31:12 +0000

    Restrict actions from ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-
-    before_action :set_user
+    
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     
     def show
         @articles = User.find(params[:id]).articles
