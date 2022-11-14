5951d5e60ff7632b274eac060676cdad8280a9eb
TiagoR79 <Tiagor79@protonmail.com>
2022-11-14 10:36:29 +0000

    Start login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+	#resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 	resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
-	#resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
