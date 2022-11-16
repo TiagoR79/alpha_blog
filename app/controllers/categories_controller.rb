0ca3e2dc885bca1dac8cae9d0d30b5dfe6bdb045
TiagoR79 <Tiagor79@protonmail.com>
2022-11-16 17:30:13 +0000

    categories create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
     def new
-        
+      @category = Category.new  
     end
     def index
         
@@ -10,6 +10,19 @@ class CategoriesController < ApplicationController
         
     end
     def create
-        
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category created successfuly"
+            redirect_to @category
+        else
+            render 'new'
+        end
     end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)    
+    end
+    
 end
