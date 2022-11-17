e930ba9ad4cfcd6776eaab05d04a7809abf042e7
TiagoR79 <Tiagor79@protonmail.com>
2022-11-17 15:49:22 +0000

    Finish setting up categories - working - ready to merge

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,31 +1,40 @@
 class CategoriesController < ApplicationController
 
-    def new
-      @category = Category.new  
-    end
+	before_action :require_admin, except: [:index, :show]
 
-    def index
-        
-    end
+	def new
+		@category = Category.new  
+	end
 
-    def create
-        @category = Category.new(category_params)
-        if @category.save
-            flash[:notice] = "Category created successfuly"
-            redirect_to @category
-        else
-            render 'new'
-        end
-    end
-    
-    def show
-      @category =  Category.find(params[:id])
-    end
+	def index
+		@categories =  Category.all
+	end
 
-    private
+	def create
+		@category = Category.new(category_params)
+		if @category.save
+			flash[:notice] = "Category created successfuly"
+			redirect_to @category
+		else
+			render 'new'
+		end
+	end
+	
+	def show
+		@category =  Category.find(params[:id])
+	end
 
-    def category_params
-        params.require(:category).permit(:name)    
-    end
+	private
+
+	def category_params
+		params.require(:category).permit(:name)    
+	end
+
+	def require_admin
+		if !(logged_in? && current_user.admin?)
+			flash[:alert] = "Only Admins can perform that action"
+			redirect_to categories_path
+		end
+	end
     
 end
